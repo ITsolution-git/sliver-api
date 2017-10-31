8e17b8cf319956c8f159f88f24dda632f832f2d6
a.karpenko <a.karpenko@smissltd.com>
2017-10-31 15:27:00 +0200

    Change schedule

--- libs/class/Scheduler.js
+++ libs/class/Scheduler.js
@@ -13,7 +13,7 @@ class Scheduler {
     }
 
     static updateSubscriptions() {
-        return schedule.scheduleJob('5 45 19 * * *', () => {
+        return schedule.scheduleJob('10 10 7 * * *', () => {
             console.log('CRON: checking subscriptions');
             Stripe.updateSubscriptions().then(() => {
                 console.log("Checking completed");

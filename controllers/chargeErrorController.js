169740589d8639ba5b8d884e7314be5c2797b444
a.karpenko <a.karpenko@smissltd.com>
2017-11-21 18:33:16 +0200

    create activity when charge failed

--- controllers/chargeErrorController.js
+++ controllers/chargeErrorController.js
@@ -3,14 +3,22 @@ const config = require('../config');
 const Mailer = require('../libs/class/Mailer');
 const mongoose = require('./../libs/mongoose');
 const User = mongoose.model('User');
+const ActivityController = require('./activityController');
 class chargeErrorController {
 
     static sendEmailNotification(req) {
         let email = req.body.data.object.receipt_email;
+        let amount = req.body.data.object.amount;
 
         return User.findOne({email:email}).then((user)=>{
             if (!user) return;
             if(user.role !=6){
+                ActivityController.create({
+                    userId: user._id,
+                    title: 'Payment Declined',
+                    type: 'Milestone',
+                    notes: amount,
+                });
                 return Mailer.renderTemplateAndSend(config.emailAdressSmallSupport, { user: user }, 'card-decline')
                 .then(() => {
                     return user;

7878dd47438485c3a751fb3fe69a65c3f237e70e
a.karpenko <a.karpenko@smissltd.com>
2017-11-24 15:44:22 +0200

    add aditional activity for report

--- controllers/chargeErrorController.js
+++ controllers/chargeErrorController.js
@@ -4,28 +4,48 @@ const Mailer = require('../libs/class/Mailer');
 const mongoose = require('./../libs/mongoose');
 const User = mongoose.model('User');
 const ActivityController = require('./activityController');
+const UserControler = require('./userController');
 class chargeErrorController {
 
     static sendEmailNotification(req) {
         let email = req.body.data.object.receipt_email;
         let amount = req.body.data.object.amount;
 
-        return User.findOne({ email: email, stripeSubscription: { $ne: null }}).then((user)=>{
-            if (!user) return;
-            if(user.role !=6){
+        return User.find({ email: email, role:4})
+        .then((users)=>{
+            if (!user.length) return;
+             UserControler.getActiveUserByEmail(users[0].email).then(userId =>{
                 ActivityController.create({
-                    userId: user._id,
+                    userId: userId,
                     title: 'Payment Declined',
                     type: 'Milestone',
                     notes: amount / 100,
                 });
                 return Mailer.renderTemplateAndSend(config.emailAdressSmallSupport, { user: user }, 'card-decline')
-                .then(() => {
-                    return user;
+                    .then(() => {
+                        return user;
                 })
-            }
+            })
+            
         })
-        
+    }
+    static chargeSuccess(req){
+        let email = req.body.data.object.receipt_email;
+        let amount = req.body.data.object.amount;
+        return User.find({ email: email, role: 4 })
+            .then((users) => {
+                if (!user.length) return;
+                 return UserControler.getActiveUserByEmail(users[0].email)
+                 .then(userId => {
+                    return ActivityController.create({
+                        userId: userId,
+                        title: 'Payment Success',
+                        type: 'Milestone',
+                        notes: amount / 100,
+                    });
+                })
+
+            })
     }
 }
 
